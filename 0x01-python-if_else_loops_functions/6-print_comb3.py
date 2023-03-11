c2bf7055f693d416731caec4f4f0fe3b62267d4a
odogwukelly <odogwukellyice@gmail.com>
2023-03-11 14:03:12 -0800

    task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 for ken in range(0, 8):
     for j in range(ken + 1, 10):
+        if ken == 8 and j == 9:
+            continue
         print('{:d}{:d}'.format(ken, j),end=', ')
-print('{:d}{:d}'.format(ken + 1, j))

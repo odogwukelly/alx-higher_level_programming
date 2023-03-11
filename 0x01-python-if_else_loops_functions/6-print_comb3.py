cecc29c03c852109c012d0d376b36255dd896b4e
odogwukelly <odogwukellyice@gmail.com>
2023-03-11 13:38:21 -0800

    task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,4 +2,4 @@
 for ken in range(0, 8):
     for j in range(ken + 1, 10):
         print('{:d}{:d}'.format(ken, j),end=', ')
-print('{:d}{:d}'.format(ken + 1, j)) 
+print('{:d}{:d}'.format(ken + 1, j))

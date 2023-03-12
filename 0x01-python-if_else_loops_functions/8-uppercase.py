ebb70adcdb8577b52a00efb68a8bdb91fb96afb6
odogwukelly <odogwukellyice@gmail.com>
2023-03-11 16:21:16 -0800

    task 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    for upper in str:
+        if ord(upper) >= 97 and ord(upper) <= 122:
+            upper = chr(ord(upper) - 32)
+        print("{:s}".format(upper), end='')
+
+    print('\n', end="")

b35bd23071dbdc2a481035a85fea6cb69171f664
odogwukelly <odogwukellyice@gmail.com>
2023-03-11 18:55:28 -0800

    advanced task

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
- #!/usr/bin/python3
- for p in range(26):
-     if p % 2 == 0:
-         print('{:c}'.format(122 - p), end='')
-     else:
-         print('{:c}'.format(90 - p), end='')
+#!/usr/bin/python3
+for i in range(26):
+    if i % 2 == 0:
+        print('{:c}'.format(122 - i), end='')
+    else:
+        print('{:c}'.format(90 - i), end='')

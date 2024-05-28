b0fd896db1cc7e658fbdc44188cb33b198ed1a7a
odogwukelly <odogwukellyice@gmail.com>
2024-05-28 15:18:01 +0100

    update variable name

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
-word_first_3 = word[:3]
-word_last_2 = word[-2:]
+first_3_word = word[0:3]
+last_2_word = word[-2:]
 middle_word = word[1:-1]
-print(f"First 3 letters: {word_first_3}")
-print(f"Last 2 letters: {word_last_2}")
+print(f"First 3 letters: {first_3_word}")
+print(f"Last 2 letters: {last_2_word}")
 print(f"Middle word: {middle_word}")

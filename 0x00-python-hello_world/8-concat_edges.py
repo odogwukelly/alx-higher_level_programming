33b7c2fb5277004ededb7f7f1d950a775bc8888c
odogwukelly <odogwukellyice@gmail.com>
2024-05-28 15:20:16 +0100

    update variable name

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
+string = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = str[39:66] + str[106:112] + str[:6]
-print(str)
+string = string[39:66] + string[106:112] + string[:6]
+print(string)

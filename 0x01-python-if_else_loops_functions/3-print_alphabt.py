93911ba483360e0ba3fa8adc3a6f9bfd256e5ee7
odogwukelly <odogwukellyice@gmail.com>
2024-05-29 03:41:00 +0100

    update on comments

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+
+# Loop through ASCII values for lowercase letters from 'a' to 'z'
 for k in range(97, 123):
+    # Skip printing 'e' and 'q'
     if chr(k) == 'e' or chr(k) == 'q':
         continue
-    print(chr(k).format(k), end='')
+    # Print the current lowercase letter
+    print(chr(k), end='')

2225e45fef88cf75d634f26fe7caf6a35195b818
odogwukelly <odogwukellyice@gmail.com>
2024-05-29 03:59:28 +0100

    update on comments

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,3 +1,6 @@
-#!/usr/bin/python3
-for ken in range(0, 99):
-    print('{0} = {1}'.format(ken, hex(ken)))
+#!/usr/bin/python3  # Shebang line to indicate that the script should be run with Python 3
+
+# Loop through numbers from 0 to 98 (inclusive)
+for k in range(0, 99):
+    # Print the number and its hexadecimal representation
+    print('{0} = {1}'.format(k, hex(k)))

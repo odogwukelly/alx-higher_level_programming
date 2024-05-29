9a42d21d20d6081a453f97c80adb7ed25c8920a5
odogwukelly <odogwukellyice@gmail.com>
2024-05-29 06:44:30 +0100

    update on comments

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,6 +1,16 @@
-#!/usr/bin/python3
+#!/usr/bin/python3  # Shebang line to indicate that the script should be run with Python 3
 
-def remove_char_at(str, n):
+def remove_char_at(string, n):
+    # Create a new string by concatenating the substring before the character at index 'n'
+    # with the substring after the character at index 'n'
+    x = string[:n] + string[n + 1:]
+
+    # Check if 'n' is negative (i.e., attempting to remove a character beyond the end of the string)
     if n < 0:
-        return (str)
-    return (str[:n] + str[n+1:])
+        # If 'n' is negative, return the original string unchanged
+        return string
+
+    # If 'n' is a valid index, return the modified string
+    return x
+
+

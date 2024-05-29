2c819b05418a791c77e06baeca48567c59f1a15c
odogwukelly <odogwukellyice@gmail.com>
2024-05-29 04:58:07 +0100

    update on comments

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,10 @@
-#!/usr/bin/python3
+#!/usr/bin/python3  # Shebang line to indicate that the script should be run with Python 3
 
 def print_last_digit(number):
-
+    # Compute the absolute value of the number and then get the last digit
     k = abs(number) % 10
+    # Print the last digit without a newline
     print(f"{k}", end='')
+    # Return the last digit
     return k
+

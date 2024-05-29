5b3bd75c6fb52ee3c91e9766edd230c10c529a71
odogwukelly <odogwukellyice@gmail.com>
2024-05-29 04:08:20 +0100

    update on comments

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,11 @@
-#!/usr/bin/python3
-for kello in range(0, 99):
-    print('{:02d}'.format(kello), end=', ')
-print('{:02d}'.format(kello + 1))
+#!/usr/bin/python3  # Shebang line to indicate that the script should be run with Python 3
+
+# Loop through numbers from 0 to 98 (inclusive)
+for k in range(0, 99):
+    # Print each number formatted as a two-digit number, followed by a comma and a space
+    print('{:02d}'.format(k), end=', ')
+
+# Print the number 99 (k + 1) formatted as a two-digit number, without a trailing comma
+print('{:02d}'.format(k + 1))
+
+

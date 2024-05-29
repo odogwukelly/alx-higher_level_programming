95a2a2261695d385f59ce92808c7c2ead05224a2
odogwukelly <odogwukellyice@gmail.com>
2024-05-29 05:38:18 +0100

    update on comments

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,17 @@
-#!/usr/bin/python3
+#!/usr/bin/python3  # Shebang line to indicate that the script should be run with Python 3
+
 def fizzbuzz():
+    # Iterate over numbers from 1 to 100
     for number in range(1, 101):
+        # Check if the number is divisible by both 3 and 5
         if number % 5 == 0 and number % 3 == 0:
-            print('FizzBuzz', end=' ')
+            print('FizzBuzz', end=' ')  # If divisible by both, print 'FizzBuzz'
+        # Check if the number is divisible by 5
         elif number % 5 == 0:
-            print('Buzz', end=' ')
+            print('Buzz', end=' ')  # If divisible by 5, print 'Buzz'
+        # Check if the number is divisible by 3
         elif number % 3 == 0:
-            print('Fizz', end=' ')
+            print('Fizz', end=' ')  # If divisible by 3, print 'Fizz'
         else:
-            print(number, end=' ')
+            print(number, end=' ')  # If not divisible by either, print the number itself
+

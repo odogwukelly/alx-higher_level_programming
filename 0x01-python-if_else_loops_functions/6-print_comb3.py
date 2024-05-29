1ba5e00ea390c669b5ce4d8458a79784483e0f57
odogwukelly <odogwukellyice@gmail.com>
2024-05-29 04:16:43 +0100

    update on comments

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,14 @@
-#!/usr/bin/python3
-for ken in range(0, 10):
-    for j in range(ken + 1, 10):
-        if ken == 8 and j == 9:
-            print('{:d}{:d}'.format(ken, j))
+#!/usr/bin/python3  # Shebang line to indicate that the script should be run with Python 3
+
+# Outer loop through numbers from 0 to 9 (inclusive)
+for k in range(0, 10):
+    # Inner loop through numbers from k+1 to 9 (inclusive)
+    for j in range(k + 1, 10):
+        # Check if the current pair is (8, 9)
+        if k == 8 and j == 9:
+            # Print the pair without a trailing comma and space
+            print('{:d}{:d}'.format(k, j))
         else:
-            print('{}{}'.format(ken, j), end=', ')
+            # Print the pair followed by a comma and space
+            print('{}{}'.format(k, j), end=', ')
+

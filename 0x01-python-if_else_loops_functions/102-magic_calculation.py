a7012e80c13699d48c9808896a74c3d1035ac2f3
odogwukelly <odogwukellyice@gmail.com>
2024-05-29 06:56:48 +0100

    update on comments

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,9 +1,15 @@
-#!/usr/bin/python3
+#!/usr/bin/python3  # Shebang line to indicate that the script should be run with Python 3
 
 def magic_calculation(a, b, c):
     """Match bytecode provided by Holberton School."""
+    # Check if 'a' is less than 'b'
     if a < b:
-        return (c)
+        return c  # If 'a' is less than 'b', return 'c'
+
+    # Check if 'c' is greater than 'b'
     if c > b:
-        return (a + b)
-    return (a*b - c)
+        return a + b  # If 'c' is greater than 'b', return the sum of 'a' and 'b'
+
+    # If none of the above conditions are met, return the result of the expression 'a * b - c'
+    return a * b - c
+

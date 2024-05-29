31ba378546c107a51ba9fdf52671593870b9cd6d
odogwukelly <odogwukellyice@gmail.com>
2024-05-29 05:41:59 +0100

    update on comments

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,12 @@
-#!/usr/bin/python3
+#!/usr/bin/python3  # Shebang line to indicate that the script should be run with Python 3
+
+# Loop through numbers from 0 to 25 (inclusive)
 for i in range(26):
+    # Check if the current number is even
     if i % 2 == 0:
-        print('{:c}'.format(122 - i), end='')
+        # Print a character corresponding to the ASCII value of (122 - i), which goes from 'z' to 'a'
+        print('{:c}'.format(122 - i), end=' ')
     else:
-        print('{:c}'.format(90 - i), end='')
+        # Print a character corresponding to the ASCII value of (90 - i), which goes from 'Z' to 'A'
+        print('{:c}'.format(90 - i), end=' ')
+
